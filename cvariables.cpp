69ed24c23611b506ab74d399e476b13a574093bb
Tom Cronin <TJCcronin@protonmail.com>
2024-02-27 15:42:19 +0000

    Add files via upload

---
+++ cvariables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// Using Variables in a sentence
+
+int myNum = 15;
+cout << "I am " << myNum << " years old. \n\n";
+
+// Using variables in a mathematical context
+
+int x = 5;
+int y = 6;
+int sum = x + y;
+cout << sum;
+
+}
